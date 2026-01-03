cba81ed3cf1e4e24bc2e770eb05ac216332f2885
Tridib Chakraborty <tridib.ewu.bd@gmail.com>
2026-01-04 04:02:25 +0600

    Books card added

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-
+import React from "react";
 
 const ErrorPage = () => {
-    return (
-        <div>
-            <h1 className="text-5xl font-bold text-red-600">404 - Page Not Found</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-red-600">404 - Page Not Found</h1>
+      <p>Status: 404</p>
+    </div>
+  );
 };
 
 export default ErrorPage;
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
-    <div className="app-container">
+    <div className="max-w-6xl mx-auto app-container">
       <NavBar></NavBar>
       <Outlet></Outlet>
       <Footer></Footer>
